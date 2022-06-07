b75d3ea333fbd980ad3453f79888bbdec35b6506
Will Greene <wgreene@ncsu.edu>
2022-06-07 17:30:35 -0400

    [Test] StudentRecordIOTest

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -0,0 +1,128 @@
+package edu.ncsu.csc216.pack_scheduler.io;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.ncsu.csc216.pack_scheduler.user.Student;
+
+class StudentRecordIOTest {
+	
+	/** row 0 of student_records.txt */
+	private String validStudent0 = "Zahir,King,zking,orci.Donec@ametmassaQuisque.com,pw,15";
+	/** row 1 of student_records.txt */
+	private String validStudent1 = "Cassandra,Schwartz,cschwartz,semper@imperdietornare.co.uk,pw,4";
+	/** row 2 of student_records.txt */
+	private String validStudent2 = "Shannon,Hansen,shansen,convallis.est.vitae@arcu.ca,pw,14";
+	/** row 3 of student_records.txt */
+	private String validStudent3 = "Demetrius,Austin,daustin,Curabitur.egestas.nunc@placeratorcilacus.co.uk,pw,18";
+	/** row 4 of student_records.txt */
+	private String validStudent4 = "Raymond,Brennan,rbrennan,litora.torquent@pellentesquemassalobortis.ca,pw,12";
+	/** row 5 of student_records.txt */
+	private String validStudent5 = "Emerald,Frost,efrost,adipiscing@acipsumPhasellus.edu,pw,3";
+	/** row 6 of student_records.txt */
+	private String validStudent6 = "Lane,Berg,lberg,sociis@non.org,pw,14";
+	/** row 7 of student_records.txt */
+	private String validStudent7 = "Griffith,Stone,gstone,porta@magnamalesuadavel.net,pw,17";
+	/** row 8 of student_records.txt */
+	private String validStudent8 = "Althea,Hicks,ahicks,Phasellus.dapibus@luctusfelis.com,pw,11";
+	/** row 9 of student_records.txt */
+	private String validStudent9 = "Dylan,Nolan,dnolan,placerat.Cras.dictum@dictum.net,pw,5";
+
+	/** list form of student_records.txt */
+	private String [] validStudents = {validStudent0, validStudent1, validStudent2, validStudent3, validStudent4, validStudent5,
+	        validStudent6, validStudent7, validStudent8, validStudent9};
+
+	/** encoded password */
+	private String hashPW;
+	/** encoded password algorithm */
+	private static final String HASH_ALGORITHM = "SHA-256";
+	
+	/**
+	 * Helper method to compare two files for the same contents
+	 * @param expFile expected output
+	 * @param actFile actual output
+	 */
+	private void checkFiles(String expFile, String actFile) {
+		try (Scanner expScanner = new Scanner(new FileInputStream(expFile));
+			 Scanner actScanner = new Scanner(new FileInputStream(actFile));) {
+			
+			while (expScanner.hasNextLine()  && actScanner.hasNextLine()) {
+				String exp = expScanner.nextLine();
+				String act = actScanner.nextLine();
+				assertEquals(exp, act, "Expected: " + exp + " Actual: " + act); 
+			}
+			
+			if (expScanner.hasNextLine()) {
+				fail("The expected results expect another line " + expScanner.nextLine());
+			}
+			if (actScanner.hasNextLine()) {
+				fail("The actual results has an extra, unexpected line: " + actScanner.nextLine());
+			}
+			
+			expScanner.close();
+			actScanner.close();
+			
+		} catch (IOException e) {
+			fail("Error reading files.");
+		}
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	    try {
+	        String password = "pw";
+	        MessageDigest digest = MessageDigest.getInstance(HASH_ALGORITHM);
+	        digest.update(password.getBytes());
+	        hashPW = Base64.getEncoder().encodeToString(digest.digest());
+	        
+	        for (int i = 0; i < validStudents.length; i++) {
+	            validStudents[i] = validStudents[i].replace(",pw,", "," + hashPW + ",");
+	        }
+	    } catch (NoSuchAlgorithmException e) {
+	        fail("Unable to create hash during setup");
+	    }
+	}
+
+	@Test
+	void testReadStudentRecords() throws FileNotFoundException {
+		String filepath = "test-files/student_records.txt";
+		String filepath2 = "test-files/invalid_student_records.txt";
+		
+		ArrayList<Student> students = StudentRecordIO.readStudentRecords(filepath);
+		
+		// testing same length
+		assertTrue(students.size() == validStudents.length);
+			
+		// testing individual items
+		for (int i = 0; i < validStudents.length; i++) {
+			if (!validStudents[i].equals(students.get(i).toString())) {
+				fail("Row " + i + " does not match");
+			}
+		}
+		
+		// testing invalid
+		ArrayList<Student> students2 = StudentRecordIO.readStudentRecords(filepath2);
+		assertTrue(students2.isEmpty());
+	}
+
+	@Test
+	void testWriteStudentRecords() {
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("Zahir", "King", "zking", "orci.Donec@ametmassaQuisque.com", hashPW, 15));
+		
+		Exception exception = assertThrows(IOException.class, 
+				() -> StudentRecordIO.writeStudentRecords("/home/sesmith5/actual_student_records.txt", students));
+		assertEquals("/home/sesmith5/actual_student_records.txt (Permission denied)", exception.getMessage());
+	}
+}
