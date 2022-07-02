396c04d8a69f1b5cdca12b4078301e3d8e4c08f0
Will Greene <wgreene@ncsu.edu>
2022-07-02 14:41:15 -0400

    [Implementation][Test] Schedule

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
@@ -3,6 +3,7 @@
  */
 package edu.ncsu.csc216.pack_scheduler.user.schedule;
 
+import edu.ncsu.csc216.pack_scheduler.course.ConflictException;
 import edu.ncsu.csc216.pack_scheduler.course.Course;
 import edu.ncsu.csc216.pack_scheduler.util.ArrayList;
 
@@ -35,9 +36,90 @@ public class Schedule {
 	 */
 	public boolean addCourseToSchedule(Course c) {
 		
+		// parameter error checking - duplicate
+		for (int i = 0; i < schedule.size(); i++) {
+			if (schedule.get(i).isDuplicate(c)) {
+				throw new IllegalArgumentException("You are already enrolled in " + c.getName() + ".");
+			}
+		}
 		
+		// parameter error checking - time conflict
+		for (int i = 0; i < schedule.size(); i++) {
+			try {
+				schedule.get(i).checkConflict(c);
+			} catch (ConflictException e) {
+				throw new IllegalArgumentException("The course cannot be added due to a conflict.");
+			}
+		}
+		
+		schedule.add(c); // might need to try + catch if duplicate error checking doesn't always throw IAE
+		
+		return true;
+	}
+	
+	/**
+	 * Removes a Course from the Schedule.
+	 * 
+	 * @param c Course to remove
+	 * @return true if Course was removed, false if there was not a Course to remove
+	 */
+	public boolean removeCourseFromSchedule(Course c) {
+		
+		for (int i = 0; i < schedule.size(); i++) {
+			if (schedule.get(i).isDuplicate(c)) {
+				schedule.remove(i);
+				return true;
+			}
+		}
 		
 		return false;
 	}
-
+	
+	/**
+	 * Resets the Schedule
+	 */
+	public void resetSchedule() {
+		schedule = new ArrayList<Course>();
+		title = "My Schedule";
+	}
+	
+	/**
+	 * Returns a 2D array of Course information from the Schedule.
+	 * 
+	 * @return a 2D array of Course information
+	 */
+	public String[][] getScheduledCourses() {
+		
+		String[][] s = new String[schedule.size()][];
+		
+		for (int i = 0; i < schedule.size(); i++) {
+			s[i] = schedule.get(i).getShortDisplayArray();
+		}
+		
+		return s;
+	}
+	
+	/**
+	 * Sets the Schedule title.
+	 * 
+	 * @param title title to set
+	 * @throws IllegalArgumentException if null title
+	 */
+	public void setTitle(String title) {
+		
+		// parameter error checking - null
+		if (title == null) {
+			throw new IllegalArgumentException("Title cannot be null");
+		}
+		
+		this.title = title;
+	}
+	
+	/**
+	 * Returns the Schedule title.
+	 * @return the Schedule title
+	 */
+	public String getTitle() {
+		return title;
+	}
 }
--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/schedule/ScheduleTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/schedule/ScheduleTest.java
@@ -3,6 +3,14 @@
  */
 package edu.ncsu.csc216.pack_scheduler.user.schedule;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.ncsu.csc216.pack_scheduler.course.Course;
+
 /**
  * Tests Schedule.java
  * 
@@ -10,6 +18,137 @@ package edu.ncsu.csc216.pack_scheduler.user.schedule;
  */
 public class ScheduleTest {
 	
+	/** Course name */
+	private static final String NAME = "CSC216";
+	/** Course title */
+	private static final String TITLE = "Software Development Fundamentals";
+	/** Course section */
+	private static final String SECTION = "001";
+	/** Course credits */
+	private static final int CREDITS = 3;
+	/** Course instructor id */
+	private static final String INSTRUCTOR_ID = "sesmith5";
+	/** Course meeting days */
+	private static final String MEETING_DAYS = "MW";
+	/** Course start time */
+	private static final int START_TIME = 1330;
+	/** Course end time */
+	private static final int END_TIME = 1445;
+	
+	/** Course name */
+	private static final String NAME_2 = "CSC116";
+	/** Course title */
+	private static final String TITLE_2 = "Intro to Programming - Java";
+	/** Course section */
+	private static final String SECTION_2 = "001";
+	/** Course credits */
+	private static final int CREDITS_2 = 3;
+	/** Course instructor id */
+	private static final String INSTRUCTOR_ID_2 = "jdyoung2";
+	/** Course meeting days */
+	private static final String MEETING_DAYS_2 = "MW";
+	/** Course start time */
+	private static final int START_TIME_2 = 910;
+	/** Course end time */
+	private static final int END_TIME_2 = 1100;
+	
 	// use CourseCatalog to load and find Courses
+	
+	/**
+	 * Tests Schedule constructor.
+	 */
+	@Test
+	public void testSchedule() {
+		Schedule s = new Schedule();
+		
+		assertEquals("My Schedule", s.getTitle());
+		
+		String[][] display = s.getScheduledCourses();
+		
+		assertThrows(IndexOutOfBoundsException.class, () -> assertEquals(null, display[0][0]));
+	}
+	
+	/**
+	 * Tests Schedule.addCourseToSchedule().
+	 */
+	@Test
+	public void testAddCourseToSchedule() {
+		Schedule s = new Schedule();
+		Course c = new Course(NAME, TITLE, SECTION, CREDITS, INSTRUCTOR_ID, MEETING_DAYS, START_TIME, END_TIME);
 
+		assertTrue(s.addCourseToSchedule(c));
+		
+		String[][] display = s.getScheduledCourses();
+		
+		assertEquals(NAME, display[0][0]);
+		assertEquals(SECTION, display[0][1]);
+		assertEquals(TITLE, display[0][2]);
+		assertEquals(c.getMeetingString(), display[0][3]);
+	}
+	
+	/**
+	 * Tests Schedule.removeCourseFromSchedule().
+	 */
+	@Test
+	public void testRemoveCourseFromSchedule() {
+		Schedule s = new Schedule();
+		Course c = new Course(NAME, TITLE, SECTION, CREDITS, INSTRUCTOR_ID, MEETING_DAYS, START_TIME, END_TIME);
+
+		s.addCourseToSchedule(c);
+		assertTrue(s.removeCourseFromSchedule(c));
+	}
+	
+	/**
+	 * Tests Schedule.resetSchedule().
+	 */
+	@Test
+	public void testResetSchedule() {
+		Schedule s = new Schedule();
+		Course c = new Course(NAME, TITLE, SECTION, CREDITS, INSTRUCTOR_ID, MEETING_DAYS, START_TIME, END_TIME);
+
+		s.setTitle("Your Schedule");
+		s.addCourseToSchedule(c);
+		s.resetSchedule();
+		
+		String[][] display = s.getScheduledCourses();
+		
+		assertEquals("My Schedule", s.getTitle());
+		assertThrows(IndexOutOfBoundsException.class, () -> assertEquals(null, display[0][0]));
+	}
+	
+	/**
+	 * Tests Schedule.getScheduledCourses().
+	 */
+	@Test
+	public void testGetScheduledCourses() {
+		Schedule s = new Schedule();
+		Course c1 = new Course(NAME, TITLE, SECTION, CREDITS, INSTRUCTOR_ID, MEETING_DAYS, START_TIME, END_TIME);
+		Course c2 = new Course(NAME_2, TITLE_2, SECTION_2, CREDITS_2, INSTRUCTOR_ID_2, MEETING_DAYS_2, START_TIME_2, END_TIME_2);
+
+		s.addCourseToSchedule(c1);
+		s.addCourseToSchedule(c2);
+		
+		String[][] display = s.getScheduledCourses();
+		
+		assertEquals(NAME, display[0][0]);
+		assertEquals(SECTION, display[0][1]);
+		assertEquals(TITLE, display[0][2]);
+		assertEquals(c1.getMeetingString(), display[0][3]);
+		
+		assertEquals(NAME_2, display[1][0]);
+		assertEquals(SECTION_2, display[1][1]);
+		assertEquals(TITLE_2, display[1][2]);
+		assertEquals(c2.getMeetingString(), display[1][3]);
+	}
+	
+	/**
+	 * Test Schedule.setTitle().
+	 */
+	@Test
+	public void testSetTitle() {
+		Schedule s = new Schedule();
+
+		s.setTitle("Your Schedule");
+		assertEquals("Your Schedule", s.getTitle());
+	}
 }
